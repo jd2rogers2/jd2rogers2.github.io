ae33b1057ac1974077d7679cae4d3469a4e9e287
jd2rogers2 <jd2rogers2@gmail.com>
2023-04-26 19:56:05 -0700

    4-26 blog

---
+++ src/staticContent/blogStartUpJournalThings/2023_4_26.js
@@ -0,0 +1,21 @@
+
+const blog = {
+    title: '2023_4_26.md',
+    content: `
+        Thanks for tuning back in avid readers (of which there are none). I want to do three things this week:
+        
+        1. start a new segment, which will be a little bit about myself; my life and my career until now and how it's helped me get to where I am.
+        2. say a bit about how I operate.
+        3. tell you how things have been going in the recent weeks.
+
+        So throughout my entire youth, my parents told me I could be anything I wanted, they supported me in anything I did, and they also gave me brain teasers and math questions etc. to fill the time on our many hours in the car rolling around national parks. This led to a sense of mediocrity with my job, a sales and marketing position at a luxury apartment community, and with my pretty standard college degree from a small school. Ever since that feeling of mediocrity washed over me, I've been mildly obsessed with never feeling that way again. 
+
+        I've gone through many iterations but I am very happy with the current state of how I manage my time in order to nullify the aforementioned mediocrity. I use a google sheets activity tracker. It's a simple list of my long term goals with a more detailed list of daily activities that help me achieve those long term goals. I make fun of people who celebrate getting to 10k steps every day, but checking one of these boxes is often the best part of my day. And I have all this broken down into a weekly and monthly goal with some macros to count the number of actions I hit for the time period. 
+
+        As an example, this is my 1 blog entry for the week, I am on track to do my 10 ab workouts, and on track to spend 10 hours working on AWS and startupy things. This week I've been learning about goroutines, go channels and concurrency, AWS's Lambda service, and I've researched and purchased some AWS cloud practitioner practice tests.
+
+        Well thanks for not stopping by! Catch ya later
+    `,
+};
+
+export default blog;
--- src/staticContent/blogStartUpJournalThings/index.js
+++ src/staticContent/blogStartUpJournalThings/index.js
@@ -1,9 +1,11 @@
 import blog1 from './2023_2_3';
 import blog2 from './2023_2_28';
 import blog3 from './2023_4_8';
+import blog4 from './2023_4_26';
 
 export {
     blog1,
     blog2,
     blog3,
+    blog4,
 };
