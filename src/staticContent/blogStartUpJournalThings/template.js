f32c9e768a8cd4da08e67fbd5c649b50c0e45099
james rogers <jd2rogers2@gmail.com>
2024-04-03 08:35:34 -0700

    content idea

--- src/staticContent/blogStartUpJournalThings/template.js
+++ src/staticContent/blogStartUpJournalThings/template.js
@@ -5,6 +5,7 @@ const blog = {
         reflect: was hackr_harvest really important on my AWS learning journey?
 
         ideas to write on:
+            dev journal (the thing i took all the leetcode notes in)
             what i've worked on in the passed week or 2
             about me segment - life career until now and past's impact
     `,
