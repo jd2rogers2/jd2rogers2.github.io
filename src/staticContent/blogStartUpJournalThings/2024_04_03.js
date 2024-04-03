2bf7e75ecd5b09103e5ba31d94a6e9e7a7fc9664
james rogers <jd2rogers2@gmail.com>
2024-04-03 07:54:32 -0700

    a terrible blog post

---
+++ src/staticContent/blogStartUpJournalThings/2024_04_03.js
@@ -0,0 +1,16 @@
+
+const blog = {
+  date: '2024_04_03',
+  title: 'job_search_prep.md',
+  content: `
+    Well here's another blog you have to suffer through that's not related to startup starting. But in order to chase our dreams we have to have a strong foundation, and right now my job is not stable.
+
+    So I've been preparing for a job search for a long time; it's probably about time I actually apply for a job. The "blind 75" set of leetcode questions is a great way to see the most important algos to study. Looking back, I'm pretty happy with how much I've leared about algos just by giving each a shot, comparing my solution to the provided optimal solutions, and taking high level notes.
+
+    Next up in the plan is system design. I'll need a course because I understand most of the components and how to piece together a system but for some reason I struggle when it comes to deciding based on tradeoffs and back of envelope calculations. I'll update you on which course I use.
+
+    I promise to not spend more than a month on this. This means that I'll be back to hackr_harvest by May.
+  `,
+};
+
+export default blog;
